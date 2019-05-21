9bb359111ceeb2673b70d1d5edeadd0ec869356a
anunkai <50621779+anunkai@users.noreply.github.com>
2019-05-21 09:19:13 -0300

    Listas em python

---
+++ lista.py
@@ -0,0 +1,12 @@
+x = 17.3
+print (int(x))
+
+lista59 = "i love you baby"
+lista15 = ["never gonna give you up", "never gonna let you down", "never gonna play around", "and hurt you"]
+
+print (lista59[0])
+
+print (lista15[0])
+print (lista15[1])
+print (lista15[2])
+print (lista15[3])
